3838899d00bf896fe19c0e978f584a79ac6a509d
Voronovs <vitalijs.voronovs@accenture.com>
2022-10-19 14:01:38 +0300

    add ClientMessage.java log message store for Client

---
+++ src/main/java/com/roadmap/clientservice/business/service/message/ClientMessage.java
@@ -0,0 +1,10 @@
+package com.roadmap.clientservice.business.service.message;
+
+public class ClientMessage {
+
+    public static final String SAVE_REQUEST_LOG = "Save Client request with body: ";
+    public static final String FIND_BY_ID_REQUEST_LOG = "Find Client by Id request with id: ";
+    public static final String UPDATE_REQUEST_LOG = "Update Client request with body: ";
+    public static final String DELETE_BY_ID_REQUEST_LOG = "Delete Client request with id: ";
+
+}
