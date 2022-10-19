8c07c4409a16632878ea095e7f376827599f0aef
Voronovs <vitalijs.voronovs@accenture.com>
2022-10-19 13:54:36 +0300

    edit exception message

--- src/main/java/com/roadmap/clientservice/business/exception/message/ExceptionMessage.java
+++ src/main/java/com/roadmap/clientservice/business/exception/message/ExceptionMessage.java
@@ -5,9 +5,6 @@ import org.springframework.stereotype.Component;
 @Component
 public class ExceptionMessage {
 
-    private ExceptionMessage() {
-    }
-
     public static final String FIELD_NOT_BLANK = "Shall not be blank";
     public static final String FIELD_NOT_NULL = "Shall not be null";
     public static final String FIELD_PAST_OR_PRESENT = "Shall be in the past or present";
@@ -15,4 +12,6 @@ public class ExceptionMessage {
     public static final String FIELD_MAX_DIGITS_25_2 = "Shall have not more, than 25 digits in integral part, " +
             "and not more, than 2 digits in decimal part";
     public static final String CLIENT_WITH_ID_NOT_FOUND = "Client not found with id: ";
+    private ExceptionMessage() {
+    }
 }
