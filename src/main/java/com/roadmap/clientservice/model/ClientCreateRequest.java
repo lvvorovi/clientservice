5a43c74ad9dc657f275309ff327d7b38cb5cc2d2
Voronovs <vitalijs.voronovs@accenture.com>
2022-10-20 10:51:03 +0300

    add static imports for log messages

--- src/main/java/com/roadmap/clientservice/model/ClientCreateRequest.java
+++ src/main/java/com/roadmap/clientservice/model/ClientCreateRequest.java
@@ -1,6 +1,6 @@
 package com.roadmap.clientservice.model;
 
-import com.roadmap.clientservice.business.validation.exception.message.ValidationExceptionMessage;
+import com.roadmap.clientservice.business.LogMessageStore;
 import lombok.Data;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.validation.annotation.Validated;
@@ -10,6 +10,8 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+import static com.roadmap.clientservice.business.LogMessageStore.*;
+
 @Data
 @Validated
 @NotNull
@@ -18,37 +20,37 @@ public class ClientCreateRequest {
     @Null
     private Long id;
 
-    @NotBlank(message = ValidationExceptionMessage.FIELD_NOT_BLANK)
-    @Size(max = 45, message = ValidationExceptionMessage.FIELD_MAX_LENGTH_45)
+    @NotBlank(message = FIELD_NOT_BLANK)
+    @Size(max = 45, message = FIELD_MAX_LENGTH_45)
     private String firstName;
 
-    @NotBlank(message = ValidationExceptionMessage.FIELD_NOT_BLANK)
-    @Size(max = 45, message = ValidationExceptionMessage.FIELD_MAX_LENGTH_45)
+    @NotBlank(message = FIELD_NOT_BLANK)
+    @Size(max = 45, message = FIELD_MAX_LENGTH_45)
     private String lastName;
 
-    @NotNull(message = ValidationExceptionMessage.FIELD_NOT_NULL)
-    @PastOrPresent(message = ValidationExceptionMessage.FIELD_PAST_OR_PRESENT)
+    @NotNull(message = FIELD_NOT_NULL)
+    @PastOrPresent(message = FIELD_PAST_OR_PRESENT)
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
     private LocalDate dateOfBirth;
 
-    @NotBlank(message = ValidationExceptionMessage.FIELD_NOT_BLANK)
-    @Size(max = 45, message = ValidationExceptionMessage.FIELD_MAX_LENGTH_45)
+    @NotBlank(message = FIELD_NOT_BLANK)
+    @Size(max = 45, message = FIELD_MAX_LENGTH_45)
     private String passportNumber;
 
-    @NotBlank(message = ValidationExceptionMessage.FIELD_NOT_BLANK)
-    @Size(max = 45, message = ValidationExceptionMessage.FIELD_MAX_LENGTH_45)
+    @NotBlank(message = FIELD_NOT_BLANK)
+    @Size(max = 45, message = FIELD_MAX_LENGTH_45)
     private String personalNumber;
 
-    @NotBlank(message = ValidationExceptionMessage.FIELD_NOT_BLANK)
-    @Size(max = 45, message = ValidationExceptionMessage.FIELD_MAX_LENGTH_45)
+    @NotBlank(message = FIELD_NOT_BLANK)
+    @Size(max = 45, message = FIELD_MAX_LENGTH_45)
     private String phoneNumber;
 
-    @NotBlank(message = ValidationExceptionMessage.FIELD_NOT_BLANK)
-    @Size(max = 45, message = ValidationExceptionMessage.FIELD_MAX_LENGTH_45)
+    @NotBlank(message = FIELD_NOT_BLANK)
+    @Size(max = 45, message = FIELD_MAX_LENGTH_45)
     private String occupation;
 
-    @NotNull(message = ValidationExceptionMessage.FIELD_NOT_NULL)
-    @Digits(integer = 25, fraction = 2, message = ValidationExceptionMessage.FIELD_MAX_DIGITS_25_2)
+    @NotNull(message = FIELD_NOT_NULL)
+    @Digits(integer = 25, fraction = 2, message = FIELD_MAX_DIGITS_25_2)
     private BigDecimal expenses;
 
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
--- src/main/java/com/roadmap/clientservice/model/ClientUpdateRequest.java
+++ src/main/java/com/roadmap/clientservice/model/ClientUpdateRequest.java
@@ -1,6 +1,6 @@
 package com.roadmap.clientservice.model;
 
-import com.roadmap.clientservice.business.validation.exception.message.ValidationExceptionMessage;
+import com.roadmap.clientservice.business.LogMessageStore;
 import lombok.Data;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.format.annotation.NumberFormat;
@@ -11,45 +11,47 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+import static com.roadmap.clientservice.business.LogMessageStore.*;
+
 @Data
 @Validated
 @NotNull
 public class ClientUpdateRequest {
 
-    @NotNull(message = ValidationExceptionMessage.FIELD_NOT_NULL)
+    @NotNull(message = FIELD_NOT_NULL)
     @NumberFormat
     private Long id;
 
-    @NotBlank(message = ValidationExceptionMessage.FIELD_NOT_BLANK)
-    @Size(max = 45, message = ValidationExceptionMessage.FIELD_MAX_LENGTH_45)
+    @NotBlank(message = FIELD_NOT_BLANK)
+    @Size(max = 45, message = FIELD_MAX_LENGTH_45)
     private String firstName;
 
-    @NotBlank(message = ValidationExceptionMessage.FIELD_NOT_BLANK)
-    @Size(max = 45, message = ValidationExceptionMessage.FIELD_MAX_LENGTH_45)
+    @NotBlank(message = FIELD_NOT_BLANK)
+    @Size(max = 45, message = FIELD_MAX_LENGTH_45)
     private String lastName;
 
-    @NotNull(message = ValidationExceptionMessage.FIELD_NOT_NULL)
-    @PastOrPresent(message = ValidationExceptionMessage.FIELD_PAST_OR_PRESENT)
+    @NotNull(message = FIELD_NOT_NULL)
+    @PastOrPresent(message = FIELD_PAST_OR_PRESENT)
     private LocalDate dateOfBirth;
 
-    @NotBlank(message = ValidationExceptionMessage.FIELD_NOT_BLANK)
-    @Size(max = 45, message = ValidationExceptionMessage.FIELD_MAX_LENGTH_45)
+    @NotBlank(message = FIELD_NOT_BLANK)
+    @Size(max = 45, message = FIELD_MAX_LENGTH_45)
     private String passportNumber;
 
-    @NotBlank(message = ValidationExceptionMessage.FIELD_NOT_BLANK)
-    @Size(max = 45, message = ValidationExceptionMessage.FIELD_MAX_LENGTH_45)
+    @NotBlank(message = FIELD_NOT_BLANK)
+    @Size(max = 45, message = FIELD_MAX_LENGTH_45)
     private String personalNumber;
 
-    @NotBlank(message = ValidationExceptionMessage.FIELD_NOT_BLANK)
-    @Size(max = 45, message = ValidationExceptionMessage.FIELD_MAX_LENGTH_45)
+    @NotBlank(message = FIELD_NOT_BLANK)
+    @Size(max = 45, message = FIELD_MAX_LENGTH_45)
     private String phoneNumber;
 
-    @NotBlank(message = ValidationExceptionMessage.FIELD_NOT_BLANK)
-    @Size(max = 45, message = ValidationExceptionMessage.FIELD_MAX_LENGTH_45)
+    @NotBlank(message = FIELD_NOT_BLANK)
+    @Size(max = 45, message = FIELD_MAX_LENGTH_45)
     private String occupation;
 
-    @NotNull(message = ValidationExceptionMessage.FIELD_NOT_NULL)
-    @Digits(integer = 25, fraction = 2, message = ValidationExceptionMessage.FIELD_MAX_DIGITS_25_2)
+    @NotNull(message = FIELD_NOT_NULL)
+    @Digits(integer = 25, fraction = 2, message = FIELD_MAX_DIGITS_25_2)
     private BigDecimal expenses;
 
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
