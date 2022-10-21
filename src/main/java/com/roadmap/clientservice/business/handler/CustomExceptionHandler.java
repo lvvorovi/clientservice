170383eec314983b60040113df58953764b0355e
Voronovs <vitalijs.voronovs@accenture.com>
2022-10-21 12:40:18 +0300

    refactor code to clean it

--- src/main/java/com/roadmap/clientservice/business/handler/CustomExceptionHandler.java
+++ src/main/java/com/roadmap/clientservice/business/handler/CustomExceptionHandler.java
@@ -3,8 +3,6 @@ package com.roadmap.clientservice.business.handler;
 import com.roadmap.clientservice.business.validation.exception.ValidationException;
 import com.roadmap.clientservice.model.ErrorDto;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -13,46 +11,53 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import java.util.stream.Collectors;
 
 import static com.roadmap.clientservice.business.LogMessageStore.VALIDATION_FAILED;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
 
 @ControllerAdvice
 @Slf4j
 public class CustomExceptionHandler {
 
-    private final MediaType jsonMediaType = MediaType.APPLICATION_JSON;
-
     @ExceptionHandler
     public ResponseEntity<ErrorDto> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
-        String message = ex.getFieldErrors().stream()
+        String exceptionMessage = ex.getFieldErrors().stream()
                 .map(error -> error.getField() + ": " + error.getDefaultMessage())
                 .collect(Collectors.joining(", "));
-        HttpStatus status = HttpStatus.BAD_REQUEST;
-        ErrorDto errorDto = new ErrorDto(status.value(), status.getReasonPhrase(), message);
-        log.warn(VALIDATION_FAILED + errorDto);
+        log.warn(VALIDATION_FAILED + exceptionMessage);
+        ErrorDto errorDto = new ErrorDto(
+                BAD_REQUEST.value(),
+                BAD_REQUEST.getReasonPhrase(),
+                exceptionMessage);
         return ResponseEntity
-                .status(status)
-                .contentType(jsonMediaType)
+                .status(BAD_REQUEST)
+                .contentType(APPLICATION_JSON)
                 .body(errorDto);
     }
 
     @ExceptionHandler
     public ResponseEntity<ErrorDto> handleClientNotFoundException(ValidationException ex) {
         log.warn(VALIDATION_FAILED + ex.getMessage());
-        HttpStatus status = HttpStatus.BAD_REQUEST;
-        ErrorDto errorDto = new ErrorDto(status.value(), status.getReasonPhrase(), ex.getMessage());
+        ErrorDto errorDto = new ErrorDto(
+                BAD_REQUEST.value(),
+                BAD_REQUEST.getReasonPhrase(),
+                ex.getMessage());
         return ResponseEntity
-                .status(status)
-                .contentType(jsonMediaType)
+                .status(BAD_REQUEST)
+                .contentType(APPLICATION_JSON)
                 .body(errorDto);
     }
 
     @ExceptionHandler
     public ResponseEntity<ErrorDto> handleError(Error ex) {
         log.error(ex.getMessage(), ex);
-        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
-        ErrorDto errorDto = new ErrorDto(status.value(), status.getReasonPhrase(), ex.getMessage());
+        ErrorDto errorDto = new ErrorDto(
+                INTERNAL_SERVER_ERROR.value(),
+                INTERNAL_SERVER_ERROR.getReasonPhrase(),
+                ex.getMessage());
         return ResponseEntity
                 .internalServerError()
-                .contentType(jsonMediaType)
+                .contentType(APPLICATION_JSON)
                 .body(errorDto);
     }
 
