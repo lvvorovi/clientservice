0deb2238892e68c06dfebb02721128bbad9a81cc
Voronovs <vitalijs.voronovs@accenture.com>
2022-10-24 18:09:25 +0300

    refactor classes:
    instead of copying fields using extentions to reuse fields and its methods from parent class
    CreateRequest(main fields) -> UpdateRequest(+ id) -> Response

--- src/main/java/com/roadmap/clientservice/model/ClientResponse.java
+++ src/main/java/com/roadmap/clientservice/model/ClientResponse.java
@@ -1,32 +1,10 @@
 package com.roadmap.clientservice.model;
 
-import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
 
-import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-
-@Data
-public class ClientResponse {
-
-    private Long id;
-
-    private String firstName;
-
-    private String lastName;
-
-    private LocalDate dateOfBirth;
-
-    private String passportNumber;
-
-    private String personalNumber;
-
-    private String phoneNumber;
-
-    private String occupation;
-
-    private BigDecimal expenses;
-
-    private LocalDateTime created;
+@ToString
+@EqualsAndHashCode(callSuper = true)
+public class ClientResponse extends ClientUpdateRequest {
 
 }
--- src/main/java/com/roadmap/clientservice/model/ClientUpdateRequest.java
+++ src/main/java/com/roadmap/clientservice/model/ClientUpdateRequest.java
@@ -1,59 +1,24 @@
 package com.roadmap.clientservice.model;
 
 import lombok.Data;
-import org.springframework.format.annotation.DateTimeFormat;
+import lombok.EqualsAndHashCode;
 import org.springframework.format.annotation.NumberFormat;
 import org.springframework.validation.annotation.Validated;
 
-import javax.validation.constraints.*;
-import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 
-import static com.roadmap.clientservice.business.LogMessageStore.*;
+import static com.roadmap.clientservice.business.LogMessageStore.FIELD_NOT_NULL;
 
 @Data
+@EqualsAndHashCode(callSuper = true)
 @Validated
 @NotNull
-public class ClientUpdateRequest {
+public class ClientUpdateRequest extends ClientCreateRequest {
 
     @NotNull(message = FIELD_NOT_NULL)
     @NumberFormat
+    @Valid
     private Long id;
-
-    @NotBlank(message = FIELD_NOT_BLANK)
-    @Size(max = 45, message = FIELD_MAX_LENGTH_45)
-    private String firstName;
-
-    @NotBlank(message = FIELD_NOT_BLANK)
-    @Size(max = 45, message = FIELD_MAX_LENGTH_45)
-    private String lastName;
-
-    @NotNull(message = FIELD_NOT_NULL)
-    @PastOrPresent(message = FIELD_PAST_OR_PRESENT)
-    private LocalDate dateOfBirth;
-
-    @NotBlank(message = FIELD_NOT_BLANK)
-    @Size(max = 45, message = FIELD_MAX_LENGTH_45)
-    private String passportNumber;
-
-    @NotBlank(message = FIELD_NOT_BLANK)
-    @Size(max = 45, message = FIELD_MAX_LENGTH_45)
-    private String personalNumber;
-
-    @NotBlank(message = FIELD_NOT_BLANK)
-    @Size(max = 45, message = FIELD_MAX_LENGTH_45)
-    private String phoneNumber;
-
-    @NotBlank(message = FIELD_NOT_BLANK)
-    @Size(max = 45, message = FIELD_MAX_LENGTH_45)
-    private String occupation;
-
-    @NotNull(message = FIELD_NOT_NULL)
-    @Digits(integer = 25, fraction = 2, message = FIELD_MAX_DIGITS_25_2)
-    private BigDecimal expenses;
-
-    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
-    private LocalDateTime created;
 
 }
