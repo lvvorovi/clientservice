6515d4a46630b212b1ba0ea3b406ac4207ce3937
Voronovs <vitalijs.voronovs@accenture.com>
2022-10-24 18:07:47 +0300

    refactor class:
     - 3 arguments in constructor instead of 5. 2 of them used 2 times.
     - change @Data to @ToString + @EqualsHashCode @Getters to have better controler of not generating getters

--- src/main/java/com/roadmap/clientservice/model/ErrorDto.java
+++ src/main/java/com/roadmap/clientservice/model/ErrorDto.java
@@ -1,22 +1,31 @@
 package com.roadmap.clientservice.model;
 
-import lombok.Data;
-import org.springframework.validation.annotation.Validated;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+import org.springframework.http.HttpStatus;
 
+import javax.servlet.http.HttpServletRequest;
 import java.time.LocalDateTime;
 
-@Data
+@EqualsAndHashCode
+@ToString
+@Getter
 public class ErrorDto {
 
     private final int code;
     private final String error;
     private final String message;
-    private LocalDateTime timeStamp;
+    private final LocalDateTime timeStamp;
+    private final String uri;
+    private final String method;
 
-    public ErrorDto(int code, String error, String message) {
-        this.code = code;
-        this.error = error;
+    public ErrorDto(HttpStatus status, String message, HttpServletRequest request) {
+        this.code = status.value();
+        this.error = status.getReasonPhrase();
         this.message = message;
+        this.uri = request.getRequestURI();
+        this.method = request.getMethod();
         this.timeStamp = LocalDateTime.now();
     }
 }
